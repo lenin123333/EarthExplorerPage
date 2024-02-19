a7a4049102a0d5d40ae9b010d8b28dbf10cab430
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-02-19 16:21:45 -0600

    new file:   css/font-awesome.css 	new file:   css/font-awesome.min.css 	modified:   css/style.css 	new file:   fonts/FontAwesome.otf 	new file:   fonts/fontawesome-webfont.eot 	new file:   fonts/fontawesome-webfont.svg 	new file:   fonts/fontawesome-webfont.ttf 	new file:   fonts/fontawesome-webfont.woff 	new file:   fonts/fontawesome-webfont.woff2 	new file:   img/Animacion.mp4 	modified:   img/Logo.png 	new file:   img/tik-tok.png 	new file:   img/youtube.svg 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,31 +1,6 @@
 //Animacion Jugador
 function iniciarAnimacion() {
-  // Lista de URLs de las imágenes a mostrar en la animación
-  const imagenes = [
-    "../img/Animaciones/idle_1.png",
-    "../img/Animaciones/idle_2.png",
-    "../img/Animaciones/idle_3.png",
-    "../img/Animaciones/idle_4.png",
-    "../img/Animaciones/idle_5.png",
-    "../img/Animaciones/idle_6.png",
-    "../img/Animaciones/idle_7.png",
-
-    // Añade aquí todas las URLs de tus imágenes
-  ];
-
-  const intervalo = 120; // Intervalo en milisegundos entre cada cambio de imagen
-  let indice = 0;
-
-  function cambiarImagen() {
-    const imagenAnimada = document.querySelector(".imagen-animada");
-    imagenAnimada.src = imagenes[indice];
-
-    // Incrementa el índice para cambiar a la siguiente imagen
-    indice = (indice + 1) % imagenes.length;
-  }
-
-  // Inicia la animación
-  setInterval(cambiarImagen, intervalo);
+ 
 }
 
 document.addEventListener("DOMContentLoaded", iniciarAnimacion);
