31ac2eb42228803843669db13ddeacc9f464a4ab
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-03-12 17:00:54 -0600

    modified:   src/Asaide.css 	modified:   src/componets/Asaide.tsx

--- src/componets/Asaide.tsx
+++ src/componets/Asaide.tsx
@@ -1,5 +1,5 @@
-// Aside.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import '../Asaide.css'
 import gif1 from '../assets/gifs/gif1.gif';
 import gif2 from '../assets/gifs/gif2.gif';
 import gif3 from '../assets/gifs/gif3.gif';
@@ -9,25 +9,48 @@ import gif6 from '../assets/gifs/gif6.gif';
 import gif7 from '../assets/gifs/gif7.gif';
 import gif8 from '../assets/gifs/gif8.gif';
 
-import '../Asaide.css'
 
-const Asaide: React.FC = () => {
+const gifs = [gif1, gif2, gif3, gif4, gif5, gif6, gif7, gif8];
+
+const Aside: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % gifs.length);
+    }, 10000); // Cambia la imagen cada 10 segundos
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <aside className="aside">
       <h2 className="aside-title">Movimientos</h2>
       <div className="gif-container">
-        <img src={gif1} alt="Movement GIF 1" />
-        <img src={gif2} alt="Movement GIF 2" />
-        <img src={gif3} alt="Movement GIF 3" />
-        <img src={gif4} alt="Movement GIF 4" />
-        <img src={gif5} alt="Movement GIF 5" />
-        <img src={gif6} alt="Movement GIF 6" />
-        <img src={gif7} alt="Movement GIF 7" />
-        <img src={gif8} alt="Movement GIF 8" />
-        
+        {gifs.map((gif, index) => (
+          <img
+            key={index}
+            src={gif}
+            alt={`Movement GIF ${index + 1}`}
+            style={{ display: index === currentIndex ? 'block' : 'none' }}
+          />
+        ))}
+        <div className="dots-container">
+          {gifs.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === currentIndex ? 'active' : ''}`}
+              onClick={() => handleClick(index)}
+            ></span>
+          ))}
+        </div>
       </div>
     </aside>
   );
 };
 
-export default Asaide;
+export default Aside;
